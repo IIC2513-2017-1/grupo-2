a1e9e90f997d05268b854364f7c90094366f05a0
Vicente Valencia <vmvalencia@uc.cl>
2017-04-30 14:23:22 -0300

    Validate users CRUD

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: {in: 6..20}, confirmation: true
 end
