85edda2f56a737f21074e3ebf8a1411c9d670120
Thomas Reisenegger <treisenegger@uc.cl>
2017-04-30 14:54:00 -0300

    Recreate schema

--- db/migrate/20170430162523_create_products.rb
+++ db/migrate/20170430162523_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-      t.string :name, null: false
+      t.string :name,      null: false
       t.text :description
-      t.integer :price, null: false
-      t.integer :stock, null: false
+      t.integer :price,    null: false
+      t.integer :stock,    null: false
       t.integer :offer
 
       t.timestamps
