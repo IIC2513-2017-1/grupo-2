769759219d7df93c6c4bf021831afe4d9aeb08d4
Thomas Reisenegger <treisenegger@uc.cl>
2017-04-30 17:43:01 -0300

    Create comment CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430192323) do
+ActiveRecord::Schema.define(version: 20170430193925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20170430192323) do
     t.integer "product_id"
     t.index ["category_id"], name: "index_categories_products_on_category_id", using: :btree
     t.index ["product_id"], name: "index_categories_products_on_product_id", using: :btree
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.index ["product_id"], name: "index_comments_on_product_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
@@ -48,4 +58,6 @@ ActiveRecord::Schema.define(version: 20170430192323) do
 
   add_foreign_key "categories_products", "categories"
   add_foreign_key "categories_products", "products"
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
 end
