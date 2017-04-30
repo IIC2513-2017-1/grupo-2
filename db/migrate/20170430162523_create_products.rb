3fbfd4fa95b437e0eac63ad4e73086ab7098af0e
Thomas Reisenegger <treisenegger@uc.cl>
2017-04-30 13:25:43 -0300

    Create product scaffold

---
+++ db/migrate/20170430162523_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.integer :stock
+      t.integer :offer
+
+      t.timestamps
+    end
+  end
+end
