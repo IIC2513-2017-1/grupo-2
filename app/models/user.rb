033243d8f99b7f9851d2673385eb119beb123ac2
Thomas Reisenegger <treisenegger@uc.cl>
2017-05-10 14:56:30 -0300

    Create user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ApplicationRecord
   has_many :purchases, dependent: :destroy
   has_many :carts
   has_many :wishes
+  belongs_to :role
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
   validates :password, length: {in: 6..20}, confirmation: true
+
+  before_validation :set_default_role
+
+  private
+  def set_default_role
+    self.role ||= Role.find_by_name('registered')
+  end
 end
