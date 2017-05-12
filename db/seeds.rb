8ba58e78a601a163cd32cf4cf1c7f859753f04ec
Thomas Reisenegger <treisenegger@uc.cl>
2017-05-12 02:10:04 -0300

    Implement grid system for product visualization

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,8 @@ end
 admin = User.new(username: "admin", email: "admin@admin.admin", password: "administrator", password_confirmation: "administrator")
 admin.role = Role.find_by_name("admin")
 admin.save
+
+product = Product.create(name: "Poké Ball", description: "Basic poké ball for catching low level and weak pokémon.", price: 200, stock: 150, offer: 0)
+product = Product.create(name: "Great Ball", description: "Elaborate poké ball for catching medium leveled pokémon.", price: 600, stock: 90, offer: 0)
+product = Product.create(name: "Ultra Ball", description: "Incredible poké ball for catching high level and strong pokémon.", price: 1200, stock: 50, offer: 0)
+product = Product.create(name: "Master Ball", description: "Perfect poké ball for catching any pokémon in one try. Recommended for use on legendary or mythical pokémon.", price: 1000000, stock: 1, offer: 0)
