1a2a8d14126e68611deee2ff397130ce768b9367
Thomas Reisenegger <treisenegger@uc.cl>
2017-05-23 23:18:08 -0400

    Fix some view elements like display of products and home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,25 @@
 class HomeController < ApplicationController
 
   def index
+    pre_products = Product.all
+    @products = []
+    pre_products.each do |product|
+      if @products.size < 3
+        @products << product
+      elsif @products[2].purchases.size < product.purchases.size
+        @products[2] = product
+        if @products[2].purchases.size > @products[1].purchases.size
+          aux_product = @products[1]
+          @products[1] = @products[2]
+          @products[2] = aux_product
+          if @products[1].purchases.size > @products[0].purchases.size
+            aux_product = @products[0]
+            @products[0] = @products[1]
+            @products[1] = aux_product
+          end
+        end
+      end
+    end
   end
 
 end
