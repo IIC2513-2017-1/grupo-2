36683200682d108def7bc24ea3dbf15fd5c88b12
Thomas Reisenegger <treisenegger@uc.cl>
2017-05-27 19:42:51 -0400

    Implement upload and display of images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -85,7 +85,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :stock, :offer, categories: [])
+      params.require(:product).permit(:name, :description, :image, :price, :stock, :offer, categories: [])
     end
 
     def find_categories
