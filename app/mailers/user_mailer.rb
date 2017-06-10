8087553c15c1dbcc0a61e5f689171b72c8c5595f
Thomas Reisenegger <treisenegger@uc.cl>
2017-06-10 02:17:16 -0400

    Implemented send email to admin on purchase creation but mail not sending

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,10 +14,11 @@ class UserMailer < ApplicationMailer
     mail(to: user.email, subject: "Amazing purchase acknowledgement")
   end
 
-  # def admin_purchase_email(user, purchase)
-  #   @user = user
-  #   @purchase = purchase
-  #   mail(to: user.email, subject: "New Amazing purchase")
-  # end
+  def admin_purchase_email(admin, user, purchase)
+    @user = user
+    @purchase = purchase
+    @admin = admin
+    mail(to: admin.email, subject: "New Amazing purchase")
+  end
 
 end
