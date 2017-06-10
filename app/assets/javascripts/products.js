b3321747eb25617a06a13939b0bd93cae30fcb97
Thomas Reisenegger <treisenegger@uc.cl>
2017-06-10 03:08:21 -0400

    Add favicon and improve sidebar style with js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,8 +3,13 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on('turbolinks:load', function () {
-  
+
   $("input.search").bind("keyup", function() {
+
+    $("div.sidebar-item").each(function() {
+      $(this).removeClass("active");
+    });
+
     var form = $("form.search");
     var url = "/products/search";
     var formData = form.serialize();
@@ -17,5 +22,17 @@ $(document).on('turbolinks:load', function () {
     $("#main-index").html(data);
   }).on('ajax:error', function (e, data) {
     console.log(data);
+  });
+
+  $("div.sidebar-item").on('click', function() {
+    var btn = $(this);
+
+    $("div.sidebar-item").each(function() {
+      $(this).removeClass("active");
+    });
+
+    $("input.search").val("");
+
+    btn.addClass("active");
   });
 });
