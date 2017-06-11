5cdda2843ca905f080ef1f044794cb2560894bba
Thomas Reisenegger <treisenegger@uc.cl>
2017-06-10 21:19:20 -0400

    Implement basic slider

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -35,4 +35,70 @@ $(document).on('turbolinks:load', function () {
 
     btn.addClass("active");
   });
+
+  var slideCount1 = $('.slider1 ul li').length;
+	var slideWidth1 = $('.slider1 ul li').width();
+	var slideHeight1 = $('.slider1 ul li').height();
+	var sliderUlWidth1 = slideCount1 * slideWidth1;
+	$('.slider1').css({ width: slideWidth1, height: slideHeight1 });
+	$('.slider1 ul').css({ width: sliderUlWidth1, marginLeft: - slideWidth1 });
+  $('.slider1 ul li:last-child').prependTo('.slider1 ul');
+
+  function moveLeft1() {
+    $('.slider1 ul').animate({
+      left: + slideWidth1
+    }, 200, function () {
+      $('.slider1 ul li:last-child').prependTo('.slider1 ul');
+      $('.slider1 ul').css('left', '');
+    });
+  };
+  function moveRight1() {
+    $('.slider1 ul').animate({
+      left: - slideWidth1
+    }, 200, function () {
+      $('.slider1 ul li:first-child').appendTo('.slider1 ul');
+      $('.slider1 ul').css('left', '');
+    });
+  };
+  $('.slider1 a.control-prev').click(function (e) {
+    e.preventDefault();
+    moveLeft1();
+  });
+  $('.slider1 a.control-next').click(function (e) {
+    e.preventDefault();
+    moveRight1();
+  });
+
+  var slideCount2 = $('.slider2 ul li').length;
+	var slideWidth2 = $('.slider2 ul li').width();
+	var slideHeight2 = $('.slider2 ul li').height();
+	var sliderUlWidth2 = slideCount2 * slideWidth2;
+	$('.slider2').css({ width: slideWidth2, height: slideHeight2 });
+	$('.slider2 ul').css({ width: sliderUlWidth2, marginLeft: - slideWidth2 });
+  $('.slider2 ul li:last-child').prependTo('.slider2 ul');
+
+  function moveLeft2() {
+    $('.slider2 ul').animate({
+      left: + slideWidth2
+    }, 200, function () {
+      $('.slider2 ul li:last-child').prependTo('.slider2 ul');
+      $('.slider2 ul').css('left', '');
+    });
+  };
+  function moveRight2() {
+    $('.slider2 ul').animate({
+      left: - slideWidth2
+    }, 200, function () {
+      $('.slider2 ul li:first-child').appendTo('.slider2 ul');
+      $('.slider2 ul').css('left', '');
+    });
+  };
+  $('.slider2 a.control-prev').click(function (e) {
+    e.preventDefault();
+    moveLeft2();
+  });
+  $('.slider2 a.control-next').click(function (e) {
+    e.preventDefault();
+    moveRight2();
+  });
 });
