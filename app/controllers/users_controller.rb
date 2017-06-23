30358398e55544b4f8e8ff8cc1b1bbc88206c1c5
Vicente Valencia <vmvalencia@uc.cl>
2017-06-23 01:21:23 -0400

    Add user profile picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -122,7 +122,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :password, :email, :password_confirmation)
+      params.require(:user).permit(:username, :password, :email, :password_confirmation, :avatar)
     end
 
     def cart_params
