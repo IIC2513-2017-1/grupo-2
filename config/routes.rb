f81b6fcfb560c8c727ed3b486af06fe71a2e92fa
Vicente Valencia <vmvalencia@uc.cl>
2017-06-23 06:57:22 -0400

    Get tweets with Amazing hashtag in json format

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
     resources :comments, :except => [:show, :new, :index]
   end
 
+  get "/tweets", to: "tweets#index"
+
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
